1d212693ab8f6c2e82f6463d07ae025a9ecb06b6
EthanRule <ethan2002@live.com>
2024-04-04 14:21:49 -0700

    init bdd code,

--- main.py
+++ main.py
@@ -1,81 +1,51 @@
-from pyeda.inter import *
+from pyeda.inter import * # pip install pyeda
+
+def getBDDs():
+    u = exprvars('u', 32)
+    v = exprvars('v', 32)
+    even = set([0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30])
+    prime = set([3, 5, 7, 11, 13, 17, 19, 23, 29, 31])
+
+    #bdd's constructed from definition in 2.
+    
+    # "For all 0 ≤ i, j ≤ 31, there is an edge from node i to node j iff (i + 3)%32 = j%32 or (i + 8)%32 = j%32"
+    rr_bdd = expr2bdd(expr(u[(u + 3) % 32] | u[(u + 8) % 32] for u in u), u, v) 
+    even_bdd = expr2bdd(expr(u in even for u in u), u)
+    prime_bdd = expr2bdd(expr(u in prime for u in u), u)
+    return rr_bdd, even_bdd, prime_bdd
+
+def testGetBDDs(rr_bdd, even_bdd, prime_bdd):
+    assert rr_bdd(27, 3)  # True
+    assert not rr_bdd(16, 20) # False
+    assert even_bdd(14)   # True
+    assert not even_bdd(13)   # False
+    assert prime_bdd(7)   # True
+    assert not prime_bdd(2)   # False
+    print("getBDDs() PASSED!")
+
+def rr2(rr_bdd):
+    rr2 = rr_bdd.compose(rr_bdd)
+    return rr2
+
+def testrr2(rr_bdd):
+    rr2_bdd = rr2(rr_bdd)
+    assert rr2_bdd(27, 6)  # True
+    assert not rr2_bdd(27, 9) # False
+    print("rr2() PASSED!")
+
+def rr2star(rr_bdd):
+    rr2star = rr2.smoothing('u', 'v')
+    return rr2star
 
 def main():
+    rr_bdd, even_bdd, prime_bdd = getBDDs()
+    testGetBDDs(rr_bdd, even_bdd, prime_bdd)
+    rrtwo = rr2()
+    testrr2(rrtwo)
+    rr2stars = rr2star()
+    print(rr2stars)
     return
-
 
 if __name__ == '__main__':
     main()
 
-#Practice Code
-    # int(True)
-    # # 1
-    # int('0')
-    # # 0
-
-    # #Creating Variable Instances
-    # a, b, c, d = map(exprvar, 'abcd')
-    # a.name, b.name, c.name, d.name
-    # print(a.name, b.name, c.name, d.name)
-
-    # #Indexing Variables
-    # x_0 = exprvar('x', 0)
-    # x_1 = exprvar('x', 1)
-    # print(x_0, x_1)
-
-    # X = exprvars('x', 8)
-    # print(X)
-    # print(X[2:5])
-
-    # #Multi Dementional Indexing
-    # Y = exprvars('y', 4, 4)
-    # print(Y)
-
-    # #Points in Boolean Space
-    #     #two coin flips
-    # x = exprvar('x')
-    # y = exprvar('y')
-    # two_flips = list(iter_points([x, y]))
-    # print(two_flips)
-    #     #three coin flips
-    # x, y, z = map(exprvar, 'xyz')
-    # three_flips = list(iter_points([x, y, z]))
-    # print(three_flips)
-
-    # #Boolean Functions
-    # X = exprvars('x', 3)
-    # f = truthtable(X, "00000001")
-    # print(f)
-    # f = truthtable2expr(f)
-    # print(f)
-    
-    # #majority wins
-    # f = truthtable(X, "00010111")
-    # print(f)
-    # f = truthtable2expr(f)
-    # print(f)
-
-    # #PyEDA Variable/Function Base Classes
-    
-    # #Boolean Variables
-    # # do NOT instantiate a Variable directly. Instead, 
-    # # use one of the concrete implementations: pyeda.boolalg.bdd.bddvar() 
-    # # pyeda.boolalg.expr.exprvar(), pyeda.boolalg.table.ttvar().
-
-    # #Boolean Functions
-    # #https://pyeda.readthedocs.io/en/latest/boolalg.html#boolean-functions
-
-    # #BDD's - https://pyeda.readthedocs.io/en/latest/bdd.html
-
-    # #Constructing BDD's
-    # print("Constructing BDD's")
-    # # 1. Convert an expression
-    # f = expr("a & b | a & c | b & c")
-    # print(f)
-    # f = expr2bdd(f)
-    # print(f)
-    # # 2. User operators on existing BDDs
-    # a, b, c = map(bddvar, 'abc')
-    # print(type(a))
-    # isinsta = isinstance(a, BinaryDecisionDiagram)
-    # print(isinsta)
