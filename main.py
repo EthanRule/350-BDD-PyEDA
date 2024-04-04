7c64923e42d5b55ea9c71a43eb7e755bdf6449a6
EthanRule <ethan2002@live.com>
2024-04-04 14:40:03 -0700

    Updated bdd generate function still dont work

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from pyeda.inter import * # pip install pyeda
 
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 def getBDDs():
     u = exprvars('u', 32)
     v = exprvars('v', 32)
@@ -9,18 +17,21 @@ def getBDDs():
     #bdd's constructed from definition in 2.
     
     # "For all 0 ≤ i, j ≤ 31, there is an edge from node i to node j iff (i + 3)%32 = j%32 or (i + 8)%32 = j%32"
-    rr_bdd = expr2bdd(expr(u[(u + 3) % 32] | u[(u + 8) % 32] for u in u), u, v) 
-    even_bdd = expr2bdd(expr(u in even for u in u), u)
-    prime_bdd = expr2bdd(expr(u in prime for u in u), u)
-    return rr_bdd, even_bdd, prime_bdd
+    rr_bdd = expr2bdd(expr(u[i] ^ v[i] for i in range(32)))
+    print("rr_bdd generated")
+    even_bdd = expr2bdd(expr(u[i] for i in range(32) if i % 2 == 0))
+    print("even_bdd generated")
+    prime_bdd = expr2bdd(expr(u[i] for i in range(32) if is_prime(i)))
+    print("prime_bdd generated")
+    return rr_bdd, even_bdd, prime_bdd, u, v
 
-def testGetBDDs(rr_bdd, even_bdd, prime_bdd):
-    assert rr_bdd(27, 3)  # True
-    assert not rr_bdd(16, 20) # False
-    assert even_bdd(14)   # True
-    assert not even_bdd(13)   # False
-    assert prime_bdd(7)   # True
-    assert not prime_bdd(2)   # False
+def testGetBDDs(rr_bdd, even_bdd, prime_bdd, u, v):
+    assert rr_bdd.restrict({u[27]: True, v[3]: True}) == BDDConstant(True)  # True
+    assert rr_bdd.restrict({u[16]: True, v[20]: True}) == BDDConstant(False) # False
+    assert even_bdd.restrict({u[14]: True})   # True
+    assert not even_bdd.restrict({u[13]: True})   # False
+    assert prime_bdd.restrict({u[7]: True})   # True
+    assert not prime_bdd.restrict({u[2]: True})   # False
     print("getBDDs() PASSED!")
 
 def rr2(rr_bdd):
@@ -38,12 +49,12 @@ def rr2star(rr_bdd):
     return rr2star
 
 def main():
-    rr_bdd, even_bdd, prime_bdd = getBDDs()
-    testGetBDDs(rr_bdd, even_bdd, prime_bdd)
-    rrtwo = rr2()
-    testrr2(rrtwo)
-    rr2stars = rr2star()
-    print(rr2stars)
+    rr_bdd, even_bdd, prime_bdd, u, v = getBDDs()
+    testGetBDDs(rr_bdd, even_bdd, prime_bdd, u, v)
+    #rrtwo = rr2()
+    #testrr2(rrtwo)
+    #rr2stars = rr2star()
+    #print(rr2stars)
     return
 
 if __name__ == '__main__':
